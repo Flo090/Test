b8c7aa56db9fdd913d3aecc9630e5c80d7d41384
Florian@Home <langflorian@msn.com>
2023-10-11 11:51:37 +0200

    #1 Beispiel fertig closes #1

--- src/20231011/KonsolenanwendungEinAusgabe/KonsolenanwendungEinAusgabe/Program.cs
+++ src/20231011/KonsolenanwendungEinAusgabe/KonsolenanwendungEinAusgabe/Program.cs
@@ -10,13 +10,21 @@ namespace KonsolenanwendungEinAusgabe
     {
         static void Main(string[] args)
         {
+            // Variablen
+            string nachname = string.Empty;
+            string vorname = string.Empty;
+
             // Eingabe der Daten
             Console.WriteLine("Ein- bzw. Ausgabe von Daten\n");
             Console.Write("Nachname: ");
+            nachname = Console.ReadLine();
 
             Console.Write("Vorname: ");
+            vorname = Console.ReadLine();
 
             // Ausgabe der Daten
+            Console.WriteLine($"\nHallo {vorname} {nachname}.");
+
             Console.ReadLine();
         }
     }
