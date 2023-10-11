6b9a434ff895a3f4715a86f8dbb55b2a141b3980
Florian@Home <langflorian@msn.com>
2023-10-11 11:47:57 +0200

    #1 Ausgabe Konsole

---
+++ src/20231011/KonsolenanwendungEinAusgabe/KonsolenanwendungEinAusgabe/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KonsolenanwendungEinAusgabe
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Eingabe der Daten
+            Console.WriteLine("Ein- bzw. Ausgabe von Daten\n");
+            Console.Write("Nachname: ");
+
+            Console.Write("Vorname: ");
+
+            // Ausgabe der Daten
+            Console.ReadLine();
+        }
+    }
+}
